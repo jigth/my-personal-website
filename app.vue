6c0916b87e289a93078612c82931045ea5b7c3d1
Daniel Ochoa <danielochoamontes@gmail.com>
2024-04-16 17:21:41 -0500

    FEAT: Add website and database backup script

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Home />
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
+
+<script setup>
+import Home from "./src/pages/Home.vue"
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello, I am {{ name }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('Daniel');
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+    text-align: center;
+}
+</style>
